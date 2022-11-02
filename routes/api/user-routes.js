aae150697195addae2031cf0ce7ef7a60db173e8
tmunkhb <tmunkhb@gmail.com>
2022-11-02 19:17:34 -0400

    verify user by email and password using compareSync

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -4,7 +4,7 @@ const { User } = require('../../models');
 // get all users
 router.get('/', (req, res) => {
   User.findAll({
-    attributes: { exclude: ['password'] }
+    // attributes: { exclude: ['password'] }
   })
     .then(dbUserData => res.json(dbUserData))
     .catch(err => {
@@ -15,7 +15,7 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   User.findOne({
-    attributes: { exclude: ['password'] },
+    // attributes: { exclude: ['password'] },
     where: {
       id: req.params.id
     }
@@ -45,6 +45,28 @@ router.post('/', (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+
+router.post('/login', (req, res) => {
+
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  }).then(dbUserData => {
+    if (!dbUserData) {
+      res.status(400).json({ message: 'No user with that email address!'});
+      return;
+    }
+    // verify user
+    const validPassword = dbUserData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect password!' });
+      return;
+    }
+    res.json({  user: dbUserData, message: 'You are now logged in!'});
+  });
 });
 
 router.put('/:id', (req, res) => {
